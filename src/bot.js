e1c6277802a5291297ae75f4c889cbd63a84dace
jessekerch <jesse.kercheval@gmail.com>
2025-01-21 08:51:03 -0800

    feat: add streaming of new messages and intents

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,51 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
+import cors from "cors";
+import express from "express";
+
 import config from "./../config/config.json" assert { type: "json" };
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const app = express();
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
+app.use(cors());
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// confirming client is ready
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log in to Discord with your client's token
+const messages = [];
+
+client.on(Events.MessageCreate, (message) => {
+  console.log("New message created: ", message.content);
+
+  if (message.content === "ping") {
+    message.channel.send("Pong");
+  }
+
+  if (!message.author.bot) {
+    messages.push({
+      author: message.author.username,
+      content: message.content,
+    });
+    console.log(`Message from ${message.author.username}: ${message.content}`);
+  }
+});
+
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+app.listen(3000, () => {
+  console.log("Backend server is running at http://localhost:3000");
+});
+
+// Log in to Discord
 client.login(config.token);
